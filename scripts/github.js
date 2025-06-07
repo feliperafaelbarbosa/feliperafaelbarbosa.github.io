29bc89201197ef557af2a5cba50c91c8302165fb
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2025-06-07 18:52:11 -0300

    Github API implementation - done

--- scripts/github.js
+++ scripts/github.js
@@ -1,6 +1,6 @@
 const username = 'feliperafaelbarbosa';
 const url = `https://api.github.com/users/${username}/repos?per_page=30`;
-var name_list_repos = ["inpi-exploratory-analysis", "energy-consumption-simulator", "i2a2-inteligencia-artificial-socioambiental-amazonia", "global-terror-predictor"];
+var listRepoName = ["inpi-exploratory-analysis", "energy-consumption-simulator", "i2a2-inteligencia-artificial-socioambiental-amazonia", "global-terror-predictor"];
 
 fetch(url)
     .then(response => {
@@ -11,8 +11,23 @@ fetch(url)
         return response.json();
     })
     .then(data => {
-        
-        
+        console.log(data);
+        data.forEach((e) => {
+            const index = listRepoName.indexOf(e.name);
+            if (index === -1) return;
+
+            const elements = document.getElementsByClassName("name-of-project");
+            const descriptionElements = document.getElementsByClassName("description-of-project");
+            const linkElements = document.getElementsByClassName("link-to-project");
+
+            if (listRepoName.includes(e.name)) {
+                if (elements[index]) {
+                    elements[index].textContent = e.name;
+                    descriptionElements[index].textContent = e.description || "Nenhuma descrição disponível.";
+                    linkElements[index].href = e.html_url;
+                }
+            }
+        });
     })
     .catch(error => {
         console.error('Erro ao acessar a API do GITHUB:', error);
